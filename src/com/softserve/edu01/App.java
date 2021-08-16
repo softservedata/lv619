59c6a8d1bad978dc02a8f0bbc835a6f788983175
Yaroslav <garasym@gmail.com>
2021-08-16 18:51:58 +0300

    Updated message

--- src/com/softserve/edu01/App.java
+++ src/com/softserve/edu01/App.java
@@ -9,6 +9,6 @@ public class App {
         for (int i = 0; i < args.length; i++) {
             System.out.println("Arg " + i + " is " + args[i]);
         }
-        System.out.println("done");
+        System.out.println("Done");
     }
 }
