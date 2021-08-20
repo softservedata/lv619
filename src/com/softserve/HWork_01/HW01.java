025a88b9775012d191848aab9fba2ff751e6e21e
Oleksii <a.a.skirko88@gmail.com>
2021-08-20 22:27:00 +0300

    Update files

--- src/com/softserve/HWork_01/HW01.java
+++ src/com/softserve/HWork_01/HW01.java
@@ -1,4 +1,37 @@
 package com.softserve.HWork_01;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class HW01 {
+    //    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//
+//        System.out.println("Hello. What is your name?");
+//        String name = br.readLine();
+//        System.out.println("Where are you live " + name + "?");
+//        String livingPlace = br.readLine();
+//
+//        System.out.println("You are " + name + " and you live in " + livingPlace);
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello. What is your name?");
+        String name = sc.nextLine();
+
+        System.out.println("Where are you live " + name + " ?");
+        String livingPlace = sc.nextLine();
+
+        System.out.println("You are " + name + " and you live in " + livingPlace);
+
+
+    }
 }
+
+
+
+
+
