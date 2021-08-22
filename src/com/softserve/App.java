30f0463ac8aaea5b4c5c02cd20964f8194c7dd67
YevhenKretovych <dragon.is@ukr.net>
2021-08-22 15:41:38 +0300

    Create YevhenKretovych branch

--- src/com/softserve/App.java
+++ src/com/softserve/App.java
@@ -5,16 +5,17 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class App {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(
-                new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(
+				new InputStreamReader(System.in));
         //
-        System.out.print("num = ");
-        String text = br.readLine();
-        int num = Integer.parseInt(text);
-        //
-        num = num + 1;
-        System.out.println("update num = " + num);
-        br.close();
-    }
+		System.out.print("num = ");
+		String text = br.readLine();
+		int num = Integer.parseInt(text);
+		//
+		num = num + 1;
+		System.out.println("update num = " + num);
+		br.close();
+
+	}
 }
---
+++ src/com/softserve/edu01/Example.java
@@ -0,0 +1,30 @@
+package com.softserve.edu01;
+
+public class Example {
+
+	private int i = 123;
+	private static int j = 211;
+	static int k = 233;
+	
+	public void myPrint() {
+		System.out.println("i = " + i);
+		System.out.println("j = " + j);
+	}
+	
+	public static void main(String[] args) {
+        System.out.println("Hello");
+        //
+        Example e1 = new Example();
+        Example e2 = new Example();
+        //
+        e1.i = 456;
+        System.out.println("e1.i = " + e1.i);
+        System.out.println("e2.i = " + e2.i);
+        //
+        e1.myPrint();
+        e2.myPrint();
+        //
+        System.out.println("main j = " +j);
+        System.out.println("e1 = " + e1.toString());
+	}
+}
