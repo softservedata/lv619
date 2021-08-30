9de344cb5c62213d2f72a78940423c9dc85ceb0b
Softservedata <garasym@gmail.com>
2021-08-30 20:04:14 +0300

    Arrays. Loop

---
+++ src/com/softserve/edu06/ApplSort.java
@@ -0,0 +1,37 @@
+package com.softserve.edu06;
+
+import java.util.Arrays;
+
+public class ApplSort {
+    public static void main(String[] args) {
+        int[] arr = { 2, -5, 7, -4, 8, 3 };
+        //int[] arr = { 4, 3, 2, 1 };
+        // int[] arr = { 1, 2, 3, 4 };
+        System.out.println("Original Array: " + Arrays.toString(arr));
+        /*-
+        boolean isSorted = true;
+        int tmp;
+        for (int i = 0; i < arr.length - 1; i++) { // i= 0, 1, 2
+            isSorted = true;
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                System.out.print(".");
+                if (arr[j] > arr[j + 1]) {
+                    tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                    isSorted = false;
+                }
+            }
+            if (isSorted) {
+                break;
+            }
+            System.out.println();
+        }
+        */
+        Arrays.sort(arr);
+        System.out.println("\nSorted Array: " + Arrays.toString(arr));
+        //
+        int index = Arrays.binarySearch(arr, 7);
+        System.out.println("index = " + index);
+    }
+}
