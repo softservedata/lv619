b5c092a2fb574b78f91a9b11f5485082a5eb00a7
YevhenKretovych <dragon.is@ukr.net>
2021-09-10 18:59:16 +0300

    Introduction H/T01

---
+++ src/com/softserve/edu01/HT01.java
@@ -0,0 +1,30 @@
+package com.softserve.edu01;
+
+public class HT01 {
+	/*
+	 * public static void main(String[] args) {
+	 * 
+	 * int a = 16; int b = 26;
+	 * 
+	 * System.out.println("a =" + a); System.out.println("b =" + b);
+	 * 
+	 * int result1 = a + b; int result2 = a - b; int result3 = a * b; int result4 =
+	 * a / b;
+	 * 
+	 * System.out.println("Плюсование = " + result1);
+	 * System.out.println("Вычитание = " + result2);
+	 * System.out.println("Умножение = " + result3); System.out.println("Деление = "
+	 * + result4); System.out.println("Как дела?");
+	 * 
+	 * String s = "answer";
+	 * 
+	 * System.out.println("s = " +s);
+	 * 
+	 * }
+	 */
+
+	public static void main(String[] args) {
+
+	}
+
+}
