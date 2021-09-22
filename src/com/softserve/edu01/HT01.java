6b8a7c1b9a6659b809a15397383e190adb62542a
YevhenKretovych <dragon.is@ukr.net>
2021-09-22 16:34:56 +0300

    Correction HT1

--- src/com/softserve/edu01/HT01.java
+++ src/com/softserve/edu01/HT01.java
@@ -1,29 +1,40 @@
 package com.softserve.edu01;
 
 public class HT01 {
-	/*
-	 * public static void main(String[] args) {
-	 * 
-	 * int a = 16; int b = 26;
-	 * 
-	 * System.out.println("a =" + a); System.out.println("b =" + b);
-	 * 
-	 * int result1 = a + b; int result2 = a - b; int result3 = a * b; int result4 =
-	 * a / b;
-	 * 
-	 * System.out.println("Плюсование = " + result1);
-	 * System.out.println("Вычитание = " + result2);
-	 * System.out.println("Умножение = " + result3); System.out.println("Деление = "
-	 * + result4); System.out.println("Как дела?");
-	 * 
-	 * String s = "answer";
-	 * 
-	 * System.out.println("s = " +s);
-	 * 
-	 * }
-	 */
 
 	public static void main(String[] args) {
+
+		int a = 16;
+		int b = 26;
+
+		System.out.println("a =" + a);
+		System.out.println("b =" + b);
+
+		int result1 = a + b;
+		int result2 = a - b;
+		int result3 = a * b;
+		int result4 = a / b;
+
+		System.out.println("Плюсование = " + result1);
+		System.out.println("Вычитание = " + result2);
+		System.out.println("Умножение = " + result3);
+		System.out.println("Деление 1 = " + result4); // false
+		System.out.println("Как дела?");
+		
+		double c = a;
+		double d = b;
+		
+		double result5 = c / d;
+		
+		System.out.println("Деление 2 = " + result5);
+
+		String s = "answer";
+
+		System.out.println("s = " + s);
+
+	}
+
+	public static void main1(String[] args) {
 
 	}
 
