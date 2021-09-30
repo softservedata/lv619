1ce1b5d7753fe54fe9e57488b8e1a54f54324897
Rostyslav <rostylka96@gmail.com>
2021-09-30 16:18:34 +0300

    Add HomeWork 10

--- src/com/softserve/hw10/App3.java
+++ src/com/softserve/hw10/App3.java
@@ -12,21 +12,19 @@ public class App3 {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String sentence = reader.readLine();
-        List<String> dollars = new ArrayList();
-        dollars = findPattern(sentence);
+        List<String> dollars = findPattern(sentence);
         for (String dollar: dollars) {
             System.out.println(dollar);
         }
         reader.close();
     }
-    public static List findPattern(String sentence){
+    public static List<String> findPattern(String sentence){
         List<String> patterns = new ArrayList<>();
-        int i = 0;
         Pattern pattern = Pattern.compile("(\\$[0-9]+.[0-9]{2})\\b");
         Matcher matcher = pattern.matcher(sentence);
         matcher.reset();
         while (matcher.find()) {
-            patterns.add(sentence.substring(matcher.start(), matcher.end()).toString());
+            patterns.add(sentence.substring(matcher.start(), matcher.end()));
         }
         return patterns;
     }
