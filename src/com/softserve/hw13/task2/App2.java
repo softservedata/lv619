4b51ed1d237dd8582db2afc8cd73c5bed2bcc92c
Rostyslav <rostylka96@gmail.com>
2021-11-08 14:39:53 +0200

    HomeWork 13

--- src/com/softserve/hw13/task2/App2.java
+++ src/com/softserve/hw13/task2/App2.java
@@ -1,12 +1,12 @@
 package com.softserve.hw13.task2;
 
 import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
-import java.util.List;
+
 
 public class App2 {
     public static void main(String[] args) {
         System.out.println(encrypt("xyz", 3));
+        System.out.println(decrypt("abc", 3));
 
     }
     public static String encrypt(String s, int n){
@@ -20,7 +20,14 @@ public class App2 {
         return new String(letters);
     }
     public static String decrypt(String s, int n){
-        return s;
+        byte [] letters = s.getBytes(StandardCharsets.UTF_8);
+        for (int i = 0; i < letters.length; i++) {
+            letters[i] -= n;
+            if (letters[i] < 97) {
+                letters[i] += 26;
+            }
+        }
+        return new String(letters);
     }
 
 }
