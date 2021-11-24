4b35a6f235c9bb32c98c5071ddc70b1ab6f847a7
Rostyslav <rostylka96@gmail.com>
2021-11-24 17:18:48 +0200

    Tasks 15 (not completed)

---
+++ src/main/java/com/softserve/rostyslav/tasks15/task1/Task1.java
@@ -0,0 +1,45 @@
+package com.softserve.rostyslav.tasks15.task1;
+
+import java.io.*;
+
+public class Task1 {
+    public static String fileName1 = ".\\src\\main\\java\\com\\softserve\\rostyslav\\tasks15\\task1\\task1.txt";
+    public static String fileName2 = ".\\src\\main\\java\\com\\softserve\\rostyslav\\tasks15\\task1\\task1result.txt";
+    public static void main(String[] args) throws IOException {
+
+        String fibonacciSequence = "1 1 ";
+        int f1 = 1;
+        int f2 = 1;
+        /*System.out.println("Input n");
+        int count = readFromConcole();*/
+        int count = readFromFile();
+        for (int i = 0; i < count - 2; i++) {
+            int f3 = f1 + f2;
+            fibonacciSequence = fibonacciSequence + f3 + " ";
+            f1 = f2;
+            f2 = f3;
+        }
+        wtiteToConcole(fibonacciSequence);
+        writeToFile(fibonacciSequence);
+
+    }
+
+    public static int readFromConcole() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        return Integer.parseInt(reader.readLine());
+    }
+
+    public static int readFromFile() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(fileName1));
+        return Integer.parseInt(reader.readLine());
+    }
+
+    public static void wtiteToConcole(String result) throws IOException {
+        System.out.println(result);
+    }
+
+    public static void writeToFile(String result) throws IOException {
+        BufferedWriter writer = new BufferedWriter(new FileWriter(fileName2));
+        writer.write(result);
+    }
+}
---
+++ src/main/java/com/softserve/rostyslav/tasks15/task2/Task2.java
@@ -0,0 +1,45 @@
+package com.softserve.rostyslav.tasks15.task2;
+
+import java.io.*;
+
+public class Task2 {
+    public static String fileName1 = ".\\src\\main\\java\\com\\softserve\\rostyslav\\tasks15\\task2\\task2.txt";
+    public static String fileName2 = ".\\src\\main\\java\\com\\softserve\\rostyslav\\tasks15\\task2\\task2result.txt";
+
+    public static void main(String[] args) throws IOException {
+    /*System.out.println("Input n");
+        int count = readFromConcole();*/
+        int number = readFromFile();
+        String result = "";
+        for (int i = 2; i <= number; i++) {
+            while (number % i == 0) {
+                result += i +"^";
+                number = number/i;
+            }
+            result = result.replace("\\^\b", ",");
+        }
+        result = result.substring(0, result.length() - 1);
+        wtiteToConcole(result);
+        writeToFile(result);
+
+    }
+
+    public static int readFromConcole() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        return Integer.parseInt(reader.readLine());
+    }
+
+    public static int readFromFile() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(fileName1));
+        return Integer.parseInt(reader.readLine());
+    }
+
+    public static void wtiteToConcole(String result) throws IOException {
+        System.out.println(result);
+    }
+
+    public static void writeToFile(String result) throws IOException {
+        BufferedWriter writer = new BufferedWriter(new FileWriter(fileName2));
+        writer.write(result);
+    }
+}
